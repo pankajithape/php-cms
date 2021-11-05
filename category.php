eb47413c18f48b8255a005046382527ba6a7092a
pankajithape <pankajithape95@gmail.com>
2021-11-05 11:56:40 +0530

    after url rewrite, image and author changes corrected

--- category.php
+++ category.php
@@ -13,9 +13,9 @@
         $post_category_id = $_GET['category'];
 
         if (is_admin($_SESSION['username'])) {
-          $stmt1 = mysqli_prepare($connection, "SELECT post_id, post_title , post_author , post_date , post_image , post_content FROM posts WHERE post_category_id=?");
+          $stmt1 = mysqli_prepare($connection, "SELECT post_id, post_title , post_user , post_date , post_image , post_content FROM posts WHERE post_category_id=?");
         } else {
-          $stmt2 = mysqli_prepare($connection, "SELECT post_id, post_title , post_author , post_date , post_image , post_content FROM posts WHERE post_category_id=$post_category_id AND post_status=?");
+          $stmt2 = mysqli_prepare($connection, "SELECT post_id, post_title , post_user , post_date , post_image , post_content FROM posts WHERE post_category_id=$post_category_id AND post_status=?");
           $published = 'published';
         }
 
@@ -23,12 +23,12 @@
         if (isset($stmt1)) {
           mysqli_stmt_bind_param($stmt1, "i", $post_category_id);
           mysqli_stmt_execute($stmt1);
-          mysqli_stmt_bind_result($stmt1, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
+          mysqli_stmt_bind_result($stmt1, $post_id, $post_title, $post_user, $post_date, $post_image, $post_content);
           $stmt = $stmt1;
         } else {
           mysqli_stmt_bind_param($stmt2, "is", $post_category_id, $published);
           mysqli_stmt_execute($stmt2);
-          mysqli_stmt_bind_result($stmt2, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
+          mysqli_stmt_bind_result($stmt2, $post_id, $post_title, $post_user, $post_date, $post_image, $post_content);
           $stmt = $stmt2;
         }
 
@@ -66,11 +66,13 @@
         <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
       </h2>
       <p class="lead">
-        by <a href="index.php"><?php echo $post_author ?></a>
+        <!-- by <a href="index.php"><?php // echo $post_author 
+                                        ?></a> -->
+        by <a href="index.php"><?php echo $post_user; ?></a>
       </p>
       <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
       <hr>
-      <img class="img-responsive" src="./images/<?php echo $post_image; ?>" alt="">
+      <img class="img-responsive" src="/cms/images/<?php echo $post_image; ?>" alt="">
       <hr>
       <p><?php echo $post_content ?>
       </p>
